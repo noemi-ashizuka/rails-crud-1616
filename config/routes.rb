506e0d94796282ac76ff463cb785b530955a657a
Noemi Ashizuka <noemi.calanducci@gmail.com>
2024-06-29 11:51:52 +0900

    morning crud

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,31 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  # HTTP_VERB "/path", to: "controller#action", as: :prefix (prefix_path)
+
+  # CRUD Read All
+  get "/restaurants", to: "restaurants#index", as: :restaurants # restaurants_path
+
+  # CRUD Create
+  # a place with a form
+  get "/restaurants/new", to: "restaurants#new", as: :new_restaurant
+  # a place for us to create the restaurant
+  post "/restaurants", to: "restaurants#create"
+
+  # CRUD Read One
+  get "/restaurants/:id", to: "restaurants#show", as: :restaurant
+
+  # CRUD UPdate
+  # form to update
+  get "/restaurants/:id/edit", to: "restaurants#edit", as: :edit_restaurant
+
+  # place to send the new info
+  patch "restaurants/:id", to: "restaurants#update"
+
+  # CRUD Delete
+
+  delete "restaurants/:id", to: "restaurants#destroy"
+
+  
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,15 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+puts "Cleaning DB..."
+Restaurant.destroy_all
+
+puts "Creating 20 restaurants..."
+
+20.times do
+  Restaurant.create!(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_name,
+    category: Faker::Restaurant.type,
+    rating: rand(0..5)
+  )
+end
+
+puts "Created #{Restaurant.count} restaurants!"
